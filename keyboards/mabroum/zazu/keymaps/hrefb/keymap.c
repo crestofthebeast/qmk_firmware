422218bfe004ba18ce5eb42bd296e55aa72492a5
avery <pekomarzu@gmail.com>
2023-03-10 12:03:06 +1100

    init personal fork

---
+++ keyboards/mabroum/zazu/keymaps/hrefb/keymap.c
@@ -0,0 +1,359 @@
+#include "repeat.h"
+#include "nshot_mod.h"
+#include "swapper.h"
+#include "keyboards/mabroum/mab_haptic.h"
+#include "keyboards/mabroum/mab_pointing.h"
+#include "keyboards/mabroum/keys.h"
+#include "lvgl.h"
+#include "display.h"
+#include "quick_tap.c"
+
+enum layers {
+    BSE = 0,
+    NAV,
+    SYM,
+    FUN,
+    UTL,
+};
+
+// layer taps
+#define SPC_NAV LT(NAV, KC_SPACE) 
+#define E_SYM LT(SYM, KC_E)
+#define QT_FUN LT(FUN, KC_QUOTE)
+#define ENT_UTL LT(UTL, KC_ENTER)
+
+
+// pointing
+#define DPI_MOD POINTER_DEFAULT_DPI_FORWARD
+#define DPI_RMOD POINTER_DEFAULT_DPI_REVERSE
+#define SNIPING SNIPING_MODE
+#define SNP_TOG SNIPING_MODE_TOGGLE
+#define DRGSCRL DRAGSCROLL_MODE
+#define DRG_TOG DRAGSCROLL_MODE_TOGGLE
+#define ACCEL_TOG ACCELERATION_TOGGLE
+
+// global
+#define SCRNSHT C(A(S(KC_P)))
+#define WRKFLW1 A(C(S(G(KC_K))))
+#define WRKFLW2 A(C(S(G(KC_Y))))
+#define RCRD A(KC_F10)
+
+// layers
+#define MO_NAV MO(1)
+#define MO_SYM MO(2)
+
+// mac
+#define MHRM_H  LGUI_T(KC_H)
+#define MHRM_T  LSFT_T(KC_T)
+#define MHRM_S  LALT_T(KC_S)
+#define MHRM_G  LCTL_T(KC_G)
+#define MHRM_N  RGUI_T(KC_N)
+#define MHRM_A  RSFT_T(KC_A)
+#define MHRM_I  RALT_T(KC_I)
+#define MHRM_CM RCTL_T(KC_COMMA)
+
+#define PSTM G(KC_V)
+#define CPYM G(KC_C)
+#define CUTM G(KC_X)
+#define SALLM G(KC_A)
+#define DWRDM A(KC_BSPC)
+#define QUITM G(KC_Q)
+#define SAVEM G(KC_S)
+
+// windows
+#define PSTW C(KC_V)
+#define CPYW C(KC_C)
+#define CUTW C(KC_X)
+#define SALLW        C(KC_A)
+#define DWRDW        C(KC_BSPC)
+#define QUITW        A(KC_F4)
+#define SAVEW        C(KC_S)
+
+#define UNDOMAC     G(KC_Z)
+#define REDOMAC     S(G(KC_Z))
+#define UNDOWIN     C(KC_Z)
+#define REDOWIN     C(KC_Y)
+#define CLIPBOARD   G(C(A(S(KC_Z))))
+#define LAUNCHER    G(KC_SPACE)
+#define RAYCAST     C(A(KC_SPACE))
+#define RAYCLIP     C(A(G(S(KC_J))))
+#define OSM_SHFT    OSM(MOD_LSFT)
+
+#include "g/keymap_combo.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [BSE] = LAYOUT_2_3x5_3(
+        KC_J, KC_J,  KC_W,   KC_F,   KC_P,   KC_B,                   KC_COLN,  KC_L,    KC_U,    KC_Y, KC_SLSH,   KC_SLSH,
+        KC_J, KC_R,  MHRM_S, MHRM_T, MHRM_H, KC_K,                   KC_UNDS,MHRM_N,  MHRM_A,  MHRM_I,    KC_O,   KC_SLSH,
+              KC_X,  KC_C,   MHRM_G, KC_D,   KC_V,                   KC_LPRN,  KC_M, MHRM_CM,  KC_DOT,  KC_DQT,
+                                QT_FUN, SPC_NAV,  KC_DOT, KC_HYPR,  E_SYM, ENT_UTL,
+                                        KC_BTN1, KC_BTN2, KC_BTN3,
+                                     // click  ,  right           , down    , left          , up
+                                     ACCEL_TOG , KC_AUDIO_VOL_DOWN, SNP_TOG, KC_AUDIO_VOL_UP, DRG_TOG
+
+    ),
+
+    [NAV] = LAYOUT_2_3x5_3(
+        KC_TRNS, KC_TRNS,  KC_CAPS,  KC_TAB,  KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_DEL,    KC_UP,   KC_HOME,  KC_TRNS,  KC_TRNS,
+        QK_BOOT, KC_PSCR,  OS_LALT, OS_LSFT,  OS_LGUI,  KC_TRNS,    KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_RIGHT,   KC_INS,  KC_TRNS,
+                 KC_TRNS,  KC_TRNS, OS_LCTL,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_BSPC,  KC_PGDN,   KC_PGUP,   KC_END,
+                                    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,
+                                                     KC_TRNS, KC_TRNS, KC_TRNS,
+                                         // click  ,  right , down      , left   , up
+                                            KC_TRNS, KC_TRNS, DPI_RMOD, KC_TRNS, DPI_MOD
+    ),
+    [SYM] = LAYOUT_2_3x5_3(
+        KC_TRNS, KC_TRNS,  KC_7,    KC_8,     KC_9,  KC_TILD,      KC_TRNS,  KC_RCBR,  KC_RBRC,     KC_AT,   KC_TRNS,  KC_TRNS,
+        KC_TRNS, OS_LALT,  KC_3,    KC_2,     KC_1,     KC_0,      KC_PLUS,  KC_LCBR,  KC_LBRC,   KC_PERC,   KC_AMPR,  KC_TRNS,
+                 KC_TRNS,  KC_4,    KC_5,     KC_6,  KC_CIRC,       KC_DLR,    KC_LT,    KC_GT,   KC_TRNS,   KC_TRNS,
+                                     KC_0,  KC_TRNS, KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,
+                                            RGB_HUD, RGB_SAI, RGB_SAD,
+                                         // click  ,  right , down      , left   , up
+                                            RGB_TOG, RGB_VAI, RGB_RMOD, RGB_VAD, RGB_MOD
+    ),
+    [FUN] = LAYOUT_2_3x5_3(
+        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
+        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
+                 KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,
+                                    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,
+                                                     KC_TRNS, KC_TRNS, KC_TRNS,
+                                         // click  ,  right , down      , left   , up
+                                            KC_TRNS, KC_TRNS, DPI_RMOD, KC_TRNS, DPI_MOD
+    ),
+    [UTL] = LAYOUT_2_3x5_3(
+        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
+        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
+                 KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,
+                                    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,
+                                                     KC_TRNS, KC_TRNS, KC_TRNS,
+                                         // click  ,  right , down      , left   , up
+                                            KC_TRNS, KC_TRNS, DPI_RMOD, KC_TRNS, DPI_MOD
+    ),                       
+
+    /*  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
+        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
+                 KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,
+                                    KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,
+                                                     KC_TRNS, KC_TRNS, KC_TRNS,
+                                         // click  ,  right , down      , left   , up
+                                            KC_TRNS, KC_TRNS, DPI_RMOD, KC_TRNS, DPI_MOD */
+};
+
+bool is_nshot_ignored_key(uint16_t keycode) {
+    switch (keycode) {
+    case MO_SYM:
+    case MO_NAV:
+    case OS_LCTL:
+    case OS_LALT:
+    case OS_RALT:
+    case OS_LGUI:
+    case OS_LSFT:
+    case SW_CG:
+        return true;
+    default:
+        return false;
+    }
+}
+
+bool is_nshot_cancel_key(uint16_t keycode) {
+    switch (keycode) {
+    case MO_SYM:
+    case MO_NAV:
+    case SW_CG:
+        return true;
+    default:
+        return false;
+    }
+}
+
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case KC_MINS:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+        case MO_SYM:
+        case MO_NAV:
+        case OS_LSFT:
+        case OS_LCTL:
+        case OS_LALT:
+        case OS_RALT:
+        case OS_LGUI:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+    }
+
+
+bool sw_win_active = false;
+
+const key_override_t comma_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMMA, KC_QUESTION);
+const key_override_t hrmcomma_override = ko_make_basic(MOD_MASK_SHIFT, MHRM_CM, KC_QUESTION);
+const key_override_t dot_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_EXLM);
+const key_override_t dqt_override = ko_make_basic(MOD_MASK_SHIFT, KC_DQT, KC_PIPE);
+const key_override_t slsh_override = ko_make_basic(MOD_MASK_SHIFT, KC_SLASH, KC_BSLS);
+const key_override_t sqt_override = ko_make_basic(MOD_MASK_SHIFT, KC_QUOTE, KC_ASTR);
+const key_override_t qt_override = ko_make_basic(MOD_MASK_SHIFT, QT_FUN, KC_ASTR);
+const key_override_t unds_override = ko_make_basic(MOD_MASK_SHIFT, KC_UNDS, KC_HASH);
+const key_override_t colon_override = ko_make_basic(MOD_MASK_SHIFT, KC_COLON, KC_SCLN);
+const key_override_t parens_override = ko_make_basic(MOD_MASK_SHIFT, KC_LPRN, KC_RPRN);
+
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &comma_override, 
+    &hrmcomma_override, 
+    &dot_override, 
+    &dqt_override, 
+    &slsh_override,
+    &sqt_override, 
+    &unds_override,
+    &colon_override, 
+    &parens_override,
+    NULL // Null terminate the array of overrides!
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    
+    process_nshot_state(keycode, record);
+    mab_process_record_haptic(keycode, record);
+    mab_process_pointing_keys(keycode, record);
+    process_repeat_key(keycode, record);
+
+    // It's important to update the mod variables *after* calling process_repeat_key, or else
+    // only a single modifier from the previous key is repeated (e.g. Ctrl+Shift+T then Repeat produces Shift+T)
+    mod_state = get_mods();
+    oneshot_mod_state = get_oneshot_mods();
+
+    
+
+    if (keymap_config.swap_lctl_lgui) {
+            update_swapper(
+                &sw_win_active, KC_LGUI, KC_TAB, SW_WIN,
+                keycode, record
+            );
+    } else {
+            update_swapper(
+                &sw_win_active, KC_LALT, KC_TAB, SW_WIN,
+                keycode, record
+            );
+    }
+    switch (keycode) {
+        case DWRD:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(DWRDM);
+                } else {
+                        tap_code16(DWRDW);
+                }
+            }
+            return false;
+        case QUIT:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(QUITM);
+                } else {
+                        tap_code16(QUITW);
+                }
+            }
+            return false;
+        case SAVE:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(SAVEM);
+                } else {
+                        tap_code16(SAVEW);
+                }
+            }
+            return false;
+        case SALL:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(SALLM);
+                } else {
+                        tap_code16(SALLW);
+                }
+            }
+            return false;
+        case COPY:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(CPYM);
+                } else {
+                        tap_code16(CPYW);
+                }
+            }
+            return false;
+        case CUT:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(CUTM);
+                } else {
+                        tap_code16(CUTW);
+                }
+            }
+            return false;
+        case PASTE:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(PSTM);
+                } else {
+                        tap_code16(PSTW);
+                }
+            }
+            return false;
+        case UNDO:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(UNDOMAC);
+                } else {
+                        tap_code16(UNDOWIN);
+                }
+            }
+            return false;
+        case REDO:
+            if (record->event.pressed) {
+                if (keymap_config.swap_lctl_lgui) {
+                        tap_code16(REDOMAC);
+                } else {
+                        tap_code16(REDOWIN);
+                }
+            }
+            return false;
+        case SW_CG:
+            if (record->event.pressed) {
+              keymap_config.raw = eeconfig_read_keymap();
+              keymap_config.swap_lctl_lgui = !keymap_config.swap_lctl_lgui;
+              keymap_config.swap_rctl_rgui = keymap_config.swap_lctl_lgui;
+              eeconfig_update_keymap(keymap_config.raw);
+              mab_set_pointer();
+              lv_event_send(ui_Layer_Indicator, USER_EVENT_ACTIVE_LAYER_CHANGE, NULL);
+            }
+            return false;
+        default:
+            return true;
+    }
+
+    if (!process_global_quick_tap(keycode, record)) {
+        return false;
+    }
+
+    return true;
+}
+
+
+
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [BSE]  =  { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)},
+    [NAV]  =  { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)},
+    [SYM]  =  { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)},
+    [FUN]  =  { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)},
+    [UTL]  =  { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)},
+};
---
+++ keyboards/mabroum/zazu/keymaps/hrefb/quick_tap.c
@@ -0,0 +1,20 @@
+// written by @dnaq.
+#if defined(global_quick_tap)
+bool process_global_quick_tap(uint16_t keycode, keyrecord_t *record) {
+    static uint16_t global_quick_tap_timer = 0;
+    if (keycode < QK_MOD_TAP || keycode > QK_MOD_TAP_MAX) {
+        global_quick_tap_timer = timer_read();
+        return true;
+    }
+    if (timer_elapsed(global_quick_tap_timer) > TAPPING_TERM) {
+        return true;
+    }
+    if (record->event.pressed) {
+        keycode                = keycode & 0xFF;
+        global_quick_tap_timer = timer_read();
+        tap_code(keycode);
+        return false;
+    }
+    return true;
+}
+#endif
